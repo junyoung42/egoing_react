79ae10e9d8a93589c669101b238dde230b28269f
junyoung <mingcoon@gmail.com>
2022-11-15 14:58:16 +0900

    map함수로 컴포넌트 호출

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
 import logo from './logo.svg';
+import ReactPlayer from 'react-player'
 
-const Header = ()=>{
+const Header = ({title})=>{
  return<header>
-  <h1><a href="index.html">WEB</a></h1>
+  <h1><a href="index.html">{title}</a></h1>
 </header>
 }
-const Nav = () =>{ 
+const Nav = ({topics}) =>{ 
+  // const liTag = [];
+  // for(let i=0; i<topics.length; i++){
+  //   liTag.push(<li key={topics[i].id}><a href={"/read/"+topics[i].id}>{topics[i].title}</a></li>)
+  // }
+
+  const liTag = topics.map((t) => <li key={t.id}><a href={"/read/" + t.id}> {t.title} </a></li>);
+
+  
   return <nav>
         <ul>
-            <li><a href="">html</a></li>
-            <li><a href="">css</a></li>
-            <li><a href="">js</a></li>
+            {liTag}
         </ul>
     </nav>
 }
-const Article = (props) =>{
-  
+const Article = ({title, body}) =>{
   return <article>
-        <h2>{props.title}</h2>
-        {props.body}
+        <h2>{title}</h2>
+        {body}
     </article>
 }
 const App = () => {
+ const topics = [ 
+  {id:1, title:'html', body:'html is ...'},
+  {id:2, title:'css', body:'css is ...'},
+  {id:3, title:'js', body:'js is ...'}, 
+]
   return (
     <div className="App">
-    <Header></Header>
-    <Nav />
-    <Article title="어서오세요!" body="웹의 세계로 초대합니다."/>
+    <Header title="웹" />
+    <Nav topics={topics} />
+    <Article title="어서오세요!" body="웹의 세계로 초대합니다." />
+
+{/* <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' /> */}
+
     
     </div>
   )}
