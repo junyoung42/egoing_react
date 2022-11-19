21ef7af4298cf89dc5366f3bd807fe170d76d83a
junyoung <mingcoon@gmail.com>
2022-11-19 15:23:44 +0900

    복습

--- src/App.js
+++ src/App.js
@@ -1,54 +1,57 @@
-import logo from './logo.svg';
-import ReactPlayer from 'react-player'
+import './App.css';
 
-const Header = ({title})=>{
- return<header>
-  <h1><a href="index.html">{title}</a></h1>
-</header>
+
+const Header =({title}) =>{
+  return <header>
+      <h1><a href ="index.html">{title}</a></h1>
+        </header>
 }
-const Nav = ({topics}) =>{ 
-  // const liTag = [];
-  // for(let i=0; i<topics.length; i++){
-  //   liTag.push(<li key={topics[i].id}><a href={"/read/"+topics[i].id}>{topics[i].title}</a></li>)
-  // }
+const Nav = ({topics}) =>{
+  
 
-  const liTag = topics.map((t) => <li key={t.id}><a href={"/read/" + t.id}> {t.id}.{t.title} <br /> {t.body} </a></li>);
-
+  const liTag  = topics.map((topics)=>
+      <li key={topics.id}><a href={"/read/"+ topics.id}>{topics.id}.{topics.title}</a> <br />{topics.body}</li>
+  )
   
   return <nav>
-        <ul>
-            {liTag}
-        </ul>
-    </nav>
+  <ul>
+    {liTag}
+  </ul>
+  </nav>  
 }
-const Article = ({title, body}) =>{
-  return <article>
-        <h2>{title}</h2>
-        {body}
-    </article>
-}
-const App = () => {
- const topics = [ 
-  {id:1, title:'html', body:'html is ...'},
-  {id:2, title:'css', body:'css is ...'},
-  {id:3, title:'js', body:'js is ...'}, 
-]
-  return (
-    <div className="App">
-    <Header title="웹" />
-    <Nav topics={topics} />  
-    <Article title="어서오세요!" body="웹의 세계로 초대합니다." />
-
-<ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' width={"1000px"} />
-
+  const Article = ({title, body}) =>{
+   
     
-    </div>
-  )}
+      return <article>
+      <h2>{title}</h2>
+      {body}
+    </article>
+  }
 
+function App() {
+  const topics = [
+    {id:1, title:'html', body:"html is ..."},
+    {id:2, title:'css', body:"css is ..."},
+    {id:3, title:'js', body:"js is ..."},
+    
+
+  ]
+
+
+  return (
+    <div className = "App">
+          <Header title="웹zz" />
+          <Nav topics = {topics} />
+          <Article title="어서오세요" body = "웹의세계로 초대" />
+       
+
+
+    </div>
+
+
+  );
+
+
+}
 
 export default App;
-
-
-
-
-
